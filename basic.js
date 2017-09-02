4810cb88763080f53e70ff752ddc1a7996dd7368
Natalie Ike <nataliesenger@Natalies-MBP.fios-router.home>
2017-09-02 16:23:40 -0700

    basic and clozecard constructors created

---
+++ basic.js
@@ -0,0 +1,23 @@
+//Constructor for Basic flashcard.  Front = question; Back = answer
+function Basic(front, back){
+	if (!(this instanceof Basic)) { 
+    return new Basic(front, back);
+  }
+  this.front = front;
+  this.back = back;
+};
+
+//Outputs Front and also returns the Front value for integrating into UI
+Basic.prototype.logFront = function(){
+  	console.log(this.front);
+  	return this.front;
+  };
+
+//Outputs Back and also returns the Back value for integrating into UI
+Basic.prototype.logBack = function(){
+  	console.log(this.back);
+  	return this.back;
+  };
+
+//Exports Basic constructor for use elsewhere
+module.exports = Basic;
---
+++ clozecard.js
@@ -0,0 +1,52 @@
+//Constructor for ClozeCard flashcard.  FullText = full text of card; Cloze = deleted part; Partial = text without Cloze
+function ClozeCard(fulltext, cloze){
+	if (!(this instanceof ClozeCard)) { 
+    return new ClozeCard(fulltext, cloze);
+  }
+
+	if(fulltext.indexOf(cloze) === -1){
+		console.log("The Cloze isn't found in the Full Text.  Please try again, or use a Basic card.")
+		return;
+	}
+
+  this.fullText = fulltext;
+  this.cloze = cloze;
+  this.partial = getPartial(fulltext, cloze);
+};
+
+var getPartial = function(fulltext, cloze){
+	var indexFront = fulltext.indexOf(cloze);
+	var indexBack = indexFront + cloze.length;
+	var partial = "";
+	if(indexFront === 0){
+		partial = "..." + fulltext.slice(indexBack + 1);
+	}
+	else if(indexBack === fulltext.length - 1){
+		partial = fulltext.slice(0, indexFront -1) + "...";
+	}
+	else{
+		partial = fulltext.slice(0, indexFront -1 ) + "..." + fulltext.slice(indexBack + 1);
+	}
+	return partial;
+};
+
+//Outputs the partial text and also returns the value for integrating into UI
+ClozeCard.prototype.logPartial = function(){
+  	console.log(this.partial);
+  	return this.partial;
+  };
+
+//Outputs the cloze text and also returns the value for integrating into UI
+ClozeCard.prototype.logCloze = function(){
+  	console.log(this.cloze);
+  	return this.cloze;
+  };
+
+//Outputs the full text and also returns the value for integrating into UI
+ClozeCard.prototype.logFullText = function(){
+  	console.log(this.fullText);
+  	return this.fullText;
+};
+
+//Exports ClozeCard constructor for use elsewhere
+module.exports = ClozeCard;
